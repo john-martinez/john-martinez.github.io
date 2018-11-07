475c3344862433bfc7723c994feb81aef293b32a
John Martinez <jrvmartinez@icloud.com>
2018-11-07 09:16:20 -0500

    fix scaling issues when resizing vertically

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ emailButton.addEventListener('click', ()=>{
 	document.execCommand("copy");	
 	alert("Email addresses successfully copied to clipboard!");
 });
+
+
+var myScrollFunc = function() {
+  var y = window.scrollY;
+  console.log("Y",y);
+};
+
+document.addEventListener("scroll", myScrollFunc);
