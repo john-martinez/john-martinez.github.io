d0f558d8333fdfe5d6c0480c17b7ff8481db3046
John Martinez <jrvmartinez@icloud.com>
2018-11-11 20:28:30 -0500

    add variables for database connection

--- models/index.js
+++ models/index.js
@@ -2,8 +2,10 @@ let mongoose = require('mongoose');
 mongoose.Promise = Promise;
 mongoose.set("debug", true);
 
+const local =  "mongodb://localhost:3000/portfolio";
+const deployed = "mongodb://admin:4r4yk0bH3!@ds121753.mlab.com:21753/portfolio";
 // connect to the database
-mongoose.connect("mongodb://admin:4r4yk0bH3!@ds121753.mlab.com:21753/portfolio");
+mongoose.connect(deployed);
 
 module.exports.Trait = require('./traits');
 module.exports.Skill = require('./skills');
