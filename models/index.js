d6af2a8f0660fa2ef0e852738925476d697cf6da
John Martinez <jrvmartinez@icloud.com>
2018-11-13 00:05:28 -0500

    remove db connect string

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ mongoose.set("debug", true);
 
 
 // connect to the database
-mongoose.connect(process.env.DB_URI);
+mongoose.connect("mongodb://admin:4r4yk0bH3!@ds121753.mlab.com:21753/portfolio");
 
 module.exports.Trait = require('./traits');
 module.exports.Skill = require('./skills');
